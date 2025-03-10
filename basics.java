da6c0350d689f5a87fd7bc6d2761160c091b9aab
AASTHASHARMA111 <saasthas111@gmail.com>
2025-03-10 23:13:29 +0530

    pattern

--- basics.java
+++ basics.java
@@ -3,5 +3,12 @@ public class basics {
         System.out.println("My first program in java");
         System.out.print("My first program in java\n");
         System.out.println("My first program in java");
+
+
+        System.out.println("Star pattern");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
     }
 }
