917cf534a83ef0522acb67c0eac007b0f656044f
AASTHASHARMA111 <saasthas111@gmail.com>
2025-04-27 22:03:46 +0530

    else_if input

--- else_if.java
+++ else_if.java
@@ -3,12 +3,16 @@ import java.util.*;
 public class else_if {
 
     public static void main(String args[]) {
-        int age=13;
 
-        if (age>=18) {
+        System.out.println("enter your age: ");
+        
+        Scanner sc = new Scanner(System.in);
+        int age= sc.nextInt();
+
+        if(age>=18) {
             System.out.println("adult");
         }
-        else if (age>=13 && age <18){
+        else if(age>=13 && age <18){
             System.out.println("teenager");
         }
         else {
