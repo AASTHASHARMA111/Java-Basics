be7eef50d74bbdd7fd596e3f7cd3eb827c4ce993
AASTHASHARMA111 <saasthas111@gmail.com>
2025-05-04 19:38:22 +0530

    while_loop1

---
+++ while_loop1.java
@@ -0,0 +1,14 @@
+// print num from 1 to 10
+
+public class while_loop1 {
+
+    public static void main(String args[]) {
+        int counter =1 ;
+        while (counter<=10) {
+            System.out.print(counter+" ");
+            counter ++;
+        }
+        System.out.println();
+    }
+    
+}
